ef0d1b323b8aa68119f77c3fc89fa9307774482c
Prashant Pandey <officialprashant.org@gmail.com>
2025-07-31 10:17:00 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 
-
 import './globals.css';
 import { ReactNode } from 'react';
 import { cn } from '@/lib/utils';
@@ -40,4 +39,3 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     </html>
   );
 }
-
